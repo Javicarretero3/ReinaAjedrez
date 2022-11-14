ecb4ef8383916dd4678f59ead1f6a67a40a6f415
javic <javic@LAPTOP-EEMLDK7I>
2022-11-14 19:13:28 +0100

    He creado los metodos set y get de fila y columna

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -1,5 +1,38 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 public class Posicion {
+	private int fila;
+	private char columna;
+	
+	public Posicion(int fila, char columna) {
+		this.fila = fila;
+		this.columna = columna;
+	}
 
+	public int getFila() {
+		return fila;
+	}
+
+	public void setFila(int fila) {
+		if (fila >= 1 || fila <= 8) {
+			throw new IllegalArgumentException("La reina esta fuera del tablero");
+	    }else {
+		    this.fila = fila; 
+	    }
+	}
+	public char getColumna() {
+		return columna;
+	}
+
+	public void setColumna(char columna) {
+		if (columna >= 1 || columna <= 6) {
+			throw new IllegalArgumentException("La reina esta fuera del tablero");
+		}else {
+			this.columna = columna;
+		}
+	}
+	
+	
+	
+	
 }
