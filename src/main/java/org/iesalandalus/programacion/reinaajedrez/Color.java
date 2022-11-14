6a6816521329de36b9d3208d53381cbf19f93c1e
javic <javic@192.168.9.223>
2022-11-14 14:19:49 +0100

    a

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Color.java
@@ -2,6 +2,9 @@ package org.iesalandalus.programacion.reinaajedrez;
 
 public enum Color {
 	BLANCO, NEGRO;
-	String cadenaMostrar;
-
+	String cadenaAMostrar;
+	
+	public String getCadenaAMostrar () {
+		return cadenaAMostrar;
+	}
 }
