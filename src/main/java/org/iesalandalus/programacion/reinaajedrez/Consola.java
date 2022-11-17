b4f139e46eb75462ecea94b2061cbdd3bbce8258
javic <javic@LAPTOP-EEMLDK7I>
2022-11-17 18:46:16 +0100

    He creado el constructor de Consola

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -2,7 +2,14 @@ package org.iesalandalus.programacion.reinaajedrez;
 
 import org.iesalandalus.programacion.utilidades.Entrada;
 
+import org.iesalandalus.programacion.reinaajedrez.Direccion;
+
+import org.iesalandalus.programacion.reinaajedrez.Color;
+
 public class Consola {
+	
+	private Consola() {
+	}
 	
 	
 
