9fbb1eb66b39d063e5ab3c72300b37cea2bbf12d
javic <javic@192.168.9.223>
2022-11-17 13:52:39 +0100

    He creado el metodo mover

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Reina.java
@@ -17,11 +17,13 @@ public class Reina {
 	public void setColor(Color color) {
 		this.color = color;
 	}
+	
 	public Posicion getPosicion() {
-		return posicion;
+		return new Posicion(posicion);
 	}
-	public void setPosicion(Posicion posicion) {
-		this.posicion = posicion;
+
+	private void setPosicion(Posicion posicion) {
+		this.posicion = new Posicion(posicion);
 	}
 	
 	public Reina(Color color) throws OperationNotSupportedException {
@@ -31,6 +33,35 @@ public class Reina {
 			posicion = new Posicion(8, 'd');
 		} else if (color == Color.BLANCO) {
 			posicion = new Posicion(1, 'd');
+		}
+	}
+	
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+		if (pasos < 1) {
+			throw new IllegalArgumentException(
+					"La reina no se puede mover con un número inferior a 1. ");
+		} else if (pasos > 7) {
+			throw new IllegalArgumentException(
+					"La reina no se pude mover con un número mayot a 7. ");
+		} else if (direccion == null) {
+			throw new NullPointerException("La direccion asignada no puede ser nula. ");
+		}
+		if (direccion == Direccion.NORTE) {
+			posicion = new Posicion(posicion.getFila() + pasos, posicion.getColumna());
+		} else if (direccion == Direccion.SUR) {
+			posicion = new Posicion(posicion.getFila() - pasos, posicion.getColumna());
+		} else if (direccion == Direccion.ESTE) {
+			posicion = new Posicion(posicion.getFila(), (char) (posicion.getColumna() + (char) pasos));
+		} else if (direccion == Direccion.OESTE) {
+			posicion = new Posicion(posicion.getFila(), (char) (posicion.getColumna() - (char) pasos));
+		} else if (direccion == Direccion.NORESTE) {
+			posicion = new Posicion(posicion.getFila() + pasos, (char) (posicion.getColumna() + (char) pasos));
+		} else if (direccion == Direccion.NOROESTE) {
+			posicion = new Posicion(posicion.getFila() + pasos, (char) (posicion.getColumna() - (char) pasos));
+		} else if (direccion == Direccion.SURESTE) {
+			posicion = new Posicion(posicion.getFila() - pasos, (char) (posicion.getColumna() + (char) pasos));
+		} else if (direccion == Direccion.SUROESTE) {
+			posicion = new Posicion(posicion.getFila() - pasos, (char) (posicion.getColumna() - (char) pasos));
 		}
 	}
 
