2d434112b36765da24f6daebdcac6cf21ded6ea5
javic <javic@192.168.9.223>
2022-11-18 10:51:18 +0100

    He creado la clase MainApp

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,8 +1,14 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
-	
+	private Reina reina;
+
 	public static void main(String[] args) {
 		
 	}
+	
+	
+
 }
