9c0d77c4c34f2f65c7cfe374405aec7559b22636
javic <javic@192.168.9.223>
2022-11-18 12:36:36 +0100

    Ultimo cambio

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,37 +1,37 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
-import org.iesalandalus.programacion.utilidades.Entrada;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Direccion;
+import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
-	
+
 	private Consola() {
 	}
-	
+
 	public static void mostrarMenu() {
-		System.out.println("1º opción: Crear reina por defecto.");
-		System.out.println("2º opción: Crear reina eligiendo el color. ");
-		System.out.println("3º opción: Mover. ");
-	    System.out.println("4º opción: Salir. ");
+		System.out.println("1º Opcion: Crear reina por defecto. ");
+		System.out.println("2º Opcion: Crear reina eligiendo color. ");
+		System.out.println("3º Opcion: Mover. ");
+		System.out.println("4º Opcion: Salir. ");
 	}
-	
+
 	public static int elegirOpcionMenu() {
 		int opcionmenu;
 		do {
-			System.out.println("Elija una de las opciones anteriores. ");
+			System.out.print("Escriba el numero de la opcion que desee elegir: ");
 			opcionmenu = Entrada.entero();
-		}while (opcionmenu < 1 || opcionmenu > 4);
+		} while (opcionmenu < 1 || opcionmenu > 5);
 		return opcionmenu;
 	}
-	
+
 	public static Color elegirColor() {
 		Color color = Color.BLANCO;
 		int elegircolor;
 		do {
-			System.out.println("Escriba el numero deseado para su reina. ");
-			System.out.println("Escriba 1 para color BLANCO. ");
-			System.out.println("Escriba 2 para color NEGRO. ");
+			System.out.println("Numero 1 para color BLANCO. ");
+			System.out.println("Numero 2 para color NEGRO. ");
+			System.out.print("Escriba el numero del color que desee elegir para su reina: ");
 			elegircolor = Entrada.entero();
 		} while (elegircolor < 1 || elegircolor > 2);
 		switch (elegircolor) {
@@ -44,64 +44,67 @@ public class Consola {
 		}
 		return color;
 	}
-	
+
 	public static void mostrarMenuDirecciones() {
-		System.out.println("Escriba 1 para mover hacia el Norte. ");
-		System.out.println("Escriba 2 para mover hacia el Noreste. ");
-		System.out.println("Escriba 3 para mover hacia el Este. ");
-		System.out.println("Escriba 4 para mover hacia el Sureste. ");
-		System.out.println("Escriba 5 para mover hacia el Sur. ");
-		System.out.println("Escriba 6 para mover hacia el Suroeste. ");
-		System.out.println("Escriba 7 para mover hacia el Oeste. ");
-		System.out.println("Escriba 8 para mover hacia el Noroeste. ");
+		System.out.println(
+				"Las direcciones a las que puede optar para mover su reina son las siguientes conjunto a los numeros a las que estan pertenecen: ");
+		System.out.println("Numero 1 para NORTE");
+		System.out.println("Numero 2 para SUR");
+		System.out.println("Numero 3 para ESTE");
+		System.out.println("Numero 4 para OESTE");
+		System.out.println("Numero 5 para NORESTE");
+		System.out.println("Numero 6 para NOROESTE");
+		System.out.println("Numero 7 para SURESTE");
+		System.out.println("Numero 8 para SUROESTE");
 	}
-	
+
 	public static Direccion elegirDireccion() {
 		Direccion direccion = Direccion.NORTE;
 		int elegirdireccion;
 		do {
-			System.out.println("Escoja un número del 1 al 8 para mover a la reina hacia la dirección deseada. ");
+			System.out.print("Escriba un número entre 1 y 8 para la direccion en la que desee moverse: ");
 			elegirdireccion = Entrada.entero();
-		}while (elegirdireccion < 1 || elegirdireccion > 8);
-		switch(elegirdireccion) {
+		} while (elegirdireccion < 1 || elegirdireccion > 8);
+		switch (elegirdireccion) {
 		case 1:
 			direccion = Direccion.NORTE;
 			break;
 		case 2:
-			direccion = Direccion.NORESTE;
+			direccion = Direccion.SUR;
 			break;
 		case 3:
 			direccion = Direccion.ESTE;
 			break;
 		case 4:
-			direccion = Direccion.SURESTE;
-			break;
-		case 5:
-			direccion = Direccion.SUR;
-			break;
-		case 6:
-			direccion = Direccion.SUROESTE;
-			break;
-		case 7:
 			direccion = Direccion.OESTE;
 			break;
-		case 8:
+		case 5:
+			direccion = Direccion.NORESTE;
+			break;
+		case 6:
 			direccion = Direccion.NOROESTE;
+			break;
+		case 7:
+			direccion = Direccion.SURESTE;
+			break;
+		case 8:
+			direccion = Direccion.SUROESTE;
 			break;
 		}
 		return direccion;
 	}
-	
-	public static int elegirPasos( ) {
+
+	public static int elegirPasos() {
 		int elegirpasos;
-	    do {
-		System.out.println("Elija un número del 1 al 7 para elegir los pasos que dara la reina. ");
-		elegirpasos = Entrada.entero();
-	}while (elegirpasos < 1 || elegirpasos > 7); 
+		do {
+			System.out.print(
+					"Escriba la cantidad de pasos entre 1 y 7 que desee que se mueva su reina: ");
+			elegirpasos = Entrada.entero();
+		} while (elegirpasos < 1 || elegirpasos > 7);
 		return elegirpasos;
 	}
-	
+
 	public static void despedirse() {
-		System.out.println("Gracias por utlizar esta aplicación. ¡Esperamos verte pronto!");
+		System.out.println("Gracias por el uso de nuestra aplicacion, espero que le haya sido de agrado. ");
 	}
 }
