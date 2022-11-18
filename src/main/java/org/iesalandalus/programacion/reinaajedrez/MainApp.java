403016163a35a238ce5e5680831c01d5ffa38333
javic <javic@192.168.9.223>
2022-11-18 10:55:51 +0100

    He creado el metodo ejecutarOpcion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -6,7 +6,36 @@ public class MainApp {
 	private Reina reina;
 
 	public static void main(String[] args) {
-		
+		MainApp.ejecutarOpcion(0);
+	}
+	
+	private static void ejecutarOpcion(int opcion) {
+		do {
+			Consola.mostrarMenu();
+			System.out.println("5º Opcion: Mostrar Reina. ");
+			opcion = Consola.elegirOpcionMenu();
+			switch (opcion) {
+			case 1:
+				crearReinaDefecto();
+				mostrarReina();
+				break;
+			case 2:
+				crearReinaColor();
+				mostrarReina();
+				break;
+			case 3:
+				mover();
+				mostrarReina();
+				break;
+			case 4:
+				Consola.despedirse();
+				break;
+			case 5:
+				mostrarReina();
+				break;
+			}
+		} while (opcion != 4);
+
 	}
 	
 	
